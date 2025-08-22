f22b885c4bb8384265309922a4eb45cdd84c3d0d
Eyüp Kerem Baş <165667589+EyupKeremBas7@users.noreply.github.com>
2025-08-22 09:54:07 +0300

    Update PackageModel.py

--- src/models/PackageModel.py
+++ src/models/PackageModel.py
@@ -99,4 +99,4 @@ class PackageConfigs(Configs):
 class PackageModel(Package):
     configs: PackageConfigs
     type: Literal["capsule"] = "capsule"
-    name: Literal["VisionAPO"] = "VisionAPI"
+    name: Literal["VisionAPI"] = "VisionAPI"
