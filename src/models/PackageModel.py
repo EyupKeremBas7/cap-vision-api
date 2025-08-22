07cc2c64188848a5f95b8d481d400df1a9b68645
Eyüp Kerem Baş <165667589+EyupKeremBas7@users.noreply.github.com>
2025-08-22 08:57:22 +0300

    Update PackageModel.py

--- src/models/PackageModel.py
+++ src/models/PackageModel.py
@@ -1,4 +1,4 @@
-from pydantic import Field,field_validator
+from pydantic import Field,validator
 from typing import List, Optional, Union, Literal
 from sdks.novavision.src.base.model import Package, Image, Inputs, Configs, Outputs, Response, Request, Output, Input, Config
 
@@ -8,7 +8,7 @@ class InputImage(Input):
     value: Union[List[Image], Image]
     type: str = "object"
 
-    @field_validator("type", pre=True, always=True)
+    @validator("type", pre=True, always=True)
     def set_type_based_on_value(cls, value, values):
         value = values.get('value')
         if isinstance(value, Image):
