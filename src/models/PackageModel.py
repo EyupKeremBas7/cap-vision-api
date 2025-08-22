51cc7789a080c7bc08304fd38e98c9b2560723f4
Eyüp Kerem Baş <165667589+EyupKeremBas7@users.noreply.github.com>
2025-08-22 09:02:06 +0300

    Update PackageModel.py

--- src/models/PackageModel.py
+++ src/models/PackageModel.py
@@ -1,4 +1,4 @@
-from pydantic import Field,validator
+from pydantic import Field, validator
 from typing import List, Optional, Union, Literal
 from sdks.novavision.src.base.model import Package, Image, Inputs, Configs, Outputs, Response, Request, Output, Input, Config
 
